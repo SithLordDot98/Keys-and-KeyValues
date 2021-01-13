84368529ece95614b9f5f648ab1193328b7d887b
SithLordDot98 <65396363+SithLordDot98@users.noreply.github.com>
2021-01-13 01:39:06 -0600

    Add files via upload
    Just trying to print out the answer after the question.

---
+++ temp.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+"""Who is This?"""
+#Starting set
+Music = {"Artist":"Pooh", "Hometown":"Memphis",
+         "Genre":"HipHop", "Song":"Twerksum", 
+         "Duration":"233", "Year":"2020", 
+         "Source":"Youtube", "RecordingLabel":"1017"}
+while(True):#True == True
+    
+    info = input.Name("This is it?\n")
+    if info is Music:
+        print(Music)
+    else:
+        print("Shoes no longer in stock!!!")
+    print(Music)
